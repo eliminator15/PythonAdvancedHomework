bfa533347fb94b8c52686db237d7f6b880692ec0
eliminator15 <119575719+eliminator15@users.noreply.github.com>
2023-05-17 23:50:43 +0900

    Create main.py

---
+++ 05/main.py
@@ -0,0 +1,14 @@
+# 수학공식을 먼저 세운 후에 코드를 작성해보세요.
+
+def solution(point):
+	if point >= 1000:
+		answer = 
+	return answer
+
+point = 2323
+ret = solution(point)
+print("solution 함수의 반환 값은", ret, "입니다.")
+
+point = 12345
+ret = solution(point)
+print("solution 함수의 반환 값은", ret, "입니다.") # 12300
